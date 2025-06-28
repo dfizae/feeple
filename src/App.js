7cc9bbbad78c8ea90f6bf1349c1f595f9c7c30f1
dfizae <gaza1268@naver.com>
2025-06-29 00:29:27 +0900

    메인 페이지  수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/request" element={<RequestPage />} />
+        <Route path=" /" element={<RequestPage />} />
       </Routes>
     </Router>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,10 +7,8 @@ function HomePage() {
     <>
       <Header />
       <main style={{ padding: "2rem" }}>
-        <h1>홈페이지에 오신 것을 환영합니다!</h1>
-        <p>여기에 소개글이나 링크 등을 넣으시면 됩니다.</p>
+        <h1>이 곳은 홈이 될 성지순례 예정지 입니다.</h1>
       </main>
-      <Footer />
     </>
   );
 }
